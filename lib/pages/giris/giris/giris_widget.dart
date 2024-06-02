fc6256ded6cbe6dfa84aff143416b956a893c86e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-02 23:07:47 +0000

    v1.0.2 ios hotfix 3

--- lib/pages/giris/giris/giris_widget.dart
+++ lib/pages/giris/giris/giris_widget.dart
@@ -246,7 +246,7 @@ class _GirisWidgetState extends State<GirisWidget>
                                   ),
                                   Text(
                                     FFLocalizations.of(context).getText(
-                                      '72y6svrw' /* v1.0.2 ios stable */,
+                                      '72y6svrw' /* v1.0.2 ios h3  */,
                                     ),
                                     textAlign: TextAlign.start,
                                     style: FlutterFlowTheme.of(context)
